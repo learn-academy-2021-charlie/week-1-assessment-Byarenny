9c11a16f3c9842433aee2db0eec350d408322e72
byarenny <byarenny@gmail.com>
2021-07-12 07:41:35 -0700

    completed week1 assessment

--- code-challenges.js
+++ code-challenges.js
@@ -10,36 +10,121 @@
 // --------------------1) Create a function that takes in a number and determines if a temperature is below boiling point, at boiling point or above boiling point. Boiling point is 212 degrees Fahrenheit.
 // Use the test variables provided below. Expected output: "35 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
-var temp1 = 35
-var temp2 = 350
-var temp3 = 212
+// var temp1 = 100
+// var temp2 = 500
+// var temp3 = 212
+
+// input: temperature
+// output: string relating to boiling point
+// Examples: 
+    // input: 100 ---> "100 is below boiling point"
+    // input: 500 ---> "500 is above boiling point"
+    // input: 212 ---> "212 is at boiling point"
+// Process:
+    // create a function named tempNum that takes in a number called temp
+    // if temp is less than 212 return "{temp} is below boiling point"
+    // if temp is greater than 212 return "{temp} is above boiling point"
+    // if temp is === 212 return "{temp} is at boiling point"
+    
+    // const tempNum = (temp) => {
+    //     if(temp === 212) {
+    //         return `${temp} is at boiling point`
+    //     }  else if (temp < 212) {
+    //         return `${temp} is below boiling point`
+    //     } else if (temp > 212) {
+    //         return `${temp} is above boiling point`
+    //     }
+    // }
+    //     console.log(tempNum(temp1))
+    //     console.log(tempNum(temp2))
+    //     console.log(tempNum(temp3))
+
 
 
 
 // --------------------2) Create a function that takes in two arrays of numbers and returns a single array with all the values sorted least to greatest.
 // Use the test variables provided below. Expected output: [-9, -7, 0, 3, 7, 8, 9, 13, 22, 36]
 
-var myNumbers1 = [3, 7, 0, 36, -9]
-var myNumbers2 = [8, -7, 22, 9, 13]
+// var myNumbers1 = [3, 7, 0, 36, -9]
+// var myNumbers2 = [8, -7, 22, 9, 13]
 
+// // // input: [3, 7, 0, 36, -9], [8, -7, 22, 9, 13]
+// // // output: [-9, -7, 0, 3, 7, 8, 9, 13, 22, 36]
+// //Process:
+//     // create a function named numArrays that takes array1 and array2. Create a variable named combinedNums with the variables myNumbers1 and myNumbers2 .concat to make a single array. Create a const named numArrays that take in array1 and array2. Return combinedNums. Use built-in method named .(sort) to sort the numbers from least to greatest in the combinedNums array.
+   
+//     // console.log(myNumbers1.concat(myNumbers2))
+//     var combinedNums = myNumbers1.concat(myNumbers2)
+  
+//     const numArrays = (array1, array2) => {
+//         return (combinedNums)
+// }
+//     console.log(combinedNums.sort((a, b) => a - b))
+
+//     console.log(numArrays([3, 7, 0, 36, -9], [8, -7, 22, 9, 13]))
+// // I do feel there could be a better way or more organized way to do this problem but I couldn't figure it out
 
 
 // --------------------3) Create a function that takes in a string of a single word and returns the string with all letters reversed.
 // Use the test variables provided below. Expected output: "ovarb", "eilrahc"
 
-var myString1 = "bravo"
-var myString2 = "charlie"
+// var myString1 = "bravo"
+// var myString2 = "charlie"
+
+// input: single string
+// output: single string with letters reversed
+// Examples: 
+    // input: "bravo" ---> "ovarb"
+    // input: "charlie" ---> "eilrahc"
+//Process:
+    //create a function named "cohort". Create a variable named "nameArray" and split the string into an array. Next, .reverse() the nameArray and lastly, .join() the array back into a string
+
+// const cohort = (name) => {
+// var nameArray = name.split("")
+//     nameArray.reverse()
+//     return nameArray.join("")
+// }
+// console.log(cohort(myString1))
+// console.log(cohort(myString2))
+
+
 
 
 
 // --------------------4) Create a function that takes in two numbers and subtracts the smaller number from the larger number.
 // Use the two sets of test variables provided below. Expected output: 15, 12
 
-var numberExample1 = 42
-var numberExample2 = 27
+// var numberExample1 = 42
+// var numberExample2 = 27
 
-var numberExample3 = 7
-var numberExample4 = 19
+// var numberExample3 = 7
+// var numberExample4 = 19
+
+// input: two numbers
+// output: the difference of the bigger number from the smaller number
+// example: 
+    // input: 42,27 ---> "15"
+    // input: 7,19 ---> "12"
+// process:
+    // create a function named "subtract" that takes in num1 and num2
+    // create if and return (if num1 > num2, return num1 - num2)
+    // create else if and return statements else if(num1 < num2, return num2 - num1, else if num1 === num2, return num1 - num2)
+
+// const subtract = (num1, num2) => {
+//     if(num1 > num2){
+//         return num1 - num2
+//     } else if(num1 < num2){
+//         return num2 - num1
+//     } else if(num1 === num2){
+//         return num1 - num2
+//     }
+// }
+    
+// console.log(subtract(42, 27))
+// console.log(subtract(7, 19))
+// console.log(subtract(4, 100))
+// console.log(subtract(45, 19))
+// console.log(subtract(3, 3))
 
 
 
@@ -47,8 +132,34 @@ var numberExample4 = 19
 // Use the two sets of test variables provided below. Expected output: "Your input is not a number", 3
 // Hint: There is an operator in JavaScript called typeof :)
 
-var numberExampleRefactor1 = 42
-var numberExampleRefactor2 = "hello"
+// var numberExampleRefactor1 = 42
+// var numberExampleRefactor2 = "hello"
 
-var numberExampleRefactor3 = 27
-var numberExampleRefactor4 = 24
+// var numberExampleRefactor3 = 27
+// var numberExampleRefactor4 = 24
+
+// input: number and word
+// output: the difference of the bigger number from the smaller number, or "your input is not a number"
+// example: 
+    // input: 42,"hello" ---> "your input is not a number"
+    // input: 27,24 ---> "3"
+// process:
+    // create a function named "subtract" that takes in num1 and num2
+    // create if and return (if num1 > num2, return num1 - num2)
+    // create else if and return statements else if(num1 < num2, return num2 - num1, else if num1 === num2, return num1 - num2)
+    // create else if statement with typeof string and return "your input is not a number"
+
+
+// const subtract = (num1, num2) => {
+//     if(num1 > num2){
+//         return num1 - num2
+//     } else if(num1 < num2){
+//         return num2 - num1
+//     } else if(num1 === num2){
+//         return num1 - num2
+//     } else if(typeof string) {
+//         return "your input is not a number"
+//     }
+// }
+//     console.log(subtract(42, "hello"))
+//     console.log(subtract(27, 24))
--- interview-questions.md
+++ interview-questions.md
@@ -7,47 +7,47 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is a function? Why would you use one?
 
-  Your answer:
+  Your answer: A function is a set data holder that can be re-used with different inputs to recieve different outputs
 
-  Researched answer:
+  Researched answer: A function is a reusable block of code that provides an output when invoked, it can perform a task or calculate a value.
 
 
 
 2. In regards to functions, what is the difference between an argument and a parameter?
 
-  Your answer:
+  Your answer: An argument is a piece of information and a parameter is what holds the information
 
-  Researched answer:
+  Researched answer: arguments are information that are stored in placeholders called parameters. There can be more than one argument as long as there is an equal number of parameters.
 
 
 
 3. What is an index? What is the difference between index and value?
 
-  Your answer:
+  Your answer: Index is the place where each character lies and value is the index as a whole
 
-  Researched answer:
+  Researched answer: index is where each value is located, strings are zero indexed and are start count at 0. Arrays are always zero indexed as well. Value is the content in the elements.
 
 
 
 4. What is a built-in method?
 
-  Your answer:
+  Your answer: A built-in method is a block of code that performs a certain output everytime you use it
 
-  Researched answer:
+  Researched answer: pre-made functionality code. Built-in methods are called globally rather than on an object.
 
 
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
-  Your answer:
+  Your answer: Pair programming is when there are two people working on a project together. One person who is driving, writes code while the other person navigates. This helps prevent errors and encourages more productivity.
 
-  Researched answer:
+  Researched answer: Pair programming involves a driver who writes code and an observer. The two programmers should switch roles frequently.
 
 
 
 6. What is something we did in class this week you found helpful?  
 
-  Your answer:
+  Your answer: I would say the most helpful thing we did in class was pair programming. It was helpful to talk about the material with someone else. I also learned a few things from the classmates that I worked with this past week.
 
 
 
@@ -55,12 +55,12 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 Research and define the following terms to the best of your ability.
 
-1. Iteration:
+1. Iteration: the process of repeating a block of code until a specific number of times or until a objective is met
 
-2. Higher Order Functions:
+2. Higher Order Functions: a function that returns another function
 
-3. Jest:
+3. Jest: a library to test javascript code
 
-4. Classes:
+4. Classes: templates for javascript objects
 
-5. React:
+5. React: a library for building user interfaces for javascript
